fcf485f4d7b677e810c379a75a41de4b99d30937
Nathan L. <nathanlpsr@gmail.com>
2026-09-07 14:59:17 -0300

    feat: add Service layer, component registration

---
+++ src/main/java/com/aprendizadojava/curso/services/UserService.java
@@ -0,0 +1,33 @@
+package com.aprendizadojava.curso.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.aprendizadojava.curso.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.aprendizadojava.curso.entities.User;
+import com.aprendizadojava.curso.repositories.UserRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
